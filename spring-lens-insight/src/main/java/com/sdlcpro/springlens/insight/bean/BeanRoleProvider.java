b729f5cda489e2bc732ffa09ff05d62fbca7cade
Kibriar Hossain <shantohossain.p@gmail.com>
2026-07-05 20:41:12 +0600

    Added BeanRoleProvider Interface to provide the BeanRole info  #106

---
+++ spring-lens-insight/src/main/java/com/sdlcpro/springlens/insight/bean/BeanRoleProvider.java
@@ -0,0 +1,8 @@
+package com.sdlcpro.springlens.insight.bean;
+
+import com.sdlcpro.springlens.model.bean.BeanRole;
+
+@FunctionalInterface
+public interface BeanRoleProvider {
+    BeanRole getBeanRole();
+}
